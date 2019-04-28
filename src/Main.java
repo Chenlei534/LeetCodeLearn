709aa0ea78247afc69c5e953b0e98c5ff382e772
chenlei <294208016@qq.com>
2019-04-28 10:32:47 +0800

    两数之和_暴力

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        int[] nums=new int[]{2, 7, 11, 15};
+        int target=9;
+        int[] result;
+        SumOfTwoNumbers sotn=new SumOfTwoNumbers();
+        result=sotn.twoSumViolence(nums,target);
+        System.out.println(result[0]+","+result[1]);
     }
 }
